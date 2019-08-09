cd57a3391b00ec1f743f61e6602613fb8ee18cea
Jon Taylor <jontaylor224@gmail.com>
2019-08-09 11:40:17 -0500

    added urls views and templates

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Author
 
-# Create your views here.
+
+def index(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipeapp/recipe_list.html', {'recipes': recipes})
+
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipeapp/recipe_detail.html', {'recipe': recipe})
+
+def author_detail(request, pk):
+    author = get_object_or_404(Author, pk=pk)
+    return render(request, 'recipeapp/author_detail.html', {'author': author})
