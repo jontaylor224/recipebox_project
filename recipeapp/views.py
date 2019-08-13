65eb662fb5c6c2d8919b22a2590747bc3f927067
Jon Taylor <jontaylor224@gmail.com>
2019-08-13 17:03:16 -0500

    added recipe list to author detail page

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -11,6 +11,10 @@ def recipe_detail(request, pk):
     recipe = get_object_or_404(Recipe, pk=pk)
     return render(request, 'recipeapp/recipe_detail.html', {'recipe': recipe})
 
+
 def author_detail(request, pk):
     author = get_object_or_404(Author, pk=pk)
-    return render(request, 'recipeapp/author_detail.html', {'author': author})
+    recipes = Recipe.objects.filter(author_id=author.id)
+    return render(request,
+                  'recipeapp/author_detail.html',
+                  {'author': author, 'recipes': recipes})
